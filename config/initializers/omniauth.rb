c3ff4b1a7c6eec4348540dfd0e392610fd4df21b
memoxmrdl <jmemox@gmail.com>
2013-03-15 11:26:50 -0600

    set key and secret use providers

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'KiZX1j6Yk0HN51UFnpVA', '5BpVtQhq1eHt3iMoVcp3BI1KTrYtDkCMdzwGvxkFys'
-  provider :github, '4f7074187cd59795a52a', '0e6e5cfdaf59f080f3d8c89699e7abe1cc046e82'
+  provider :twitter, 'ID', 'SECRET'
+  provider :github, 'ID', 'SECRET'
 end
